3c891624e4065e251330d8dd2c0655fd35f5e957
sylvester <nkwontasylvester15@gmail.com>
2022-01-20 16:48:24 -0900

    CORS added

--- backend/settings.py
+++ backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogpost',
+    'graphene_django',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
